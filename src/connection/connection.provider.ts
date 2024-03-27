a5e655720dfa7ac7261cfc27a3f58f6779c66d19
Cristian Tejeda <do.cristian.tr@hotmail.com>
2024-03-27 01:40:27 -0400

    fix: security issues

--- src/connection/connection.provider.ts
+++ src/connection/connection.provider.ts
@@ -1,15 +1,22 @@
 import { DynamicModule } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+import { Environment } from "../common/enums/environment.enum";
 
 export const ConnectionProvider: DynamicModule = TypeOrmModule.forRootAsync({
     inject: [ConfigService],
-    useFactory: async (config: ConfigService) => ({
-        type: 'mongodb',
-        url: config.get('DB_URI'),
-        ssl: true,
-        autoLoadEntities: true,
-        synchronize: true,
-        logging: true
-    })
+    async useFactory (config: ConfigService) {
+        const isDev: boolean = this.config.get('NODE_ENV') !== Environment.Production
+        const connection: TypeOrmModuleOptions = {
+            type: 'mongodb',
+            url: config.get('DB_URI'),
+            ssl: true,
+            autoLoadEntities: true,
+            synchronize: isDev,
+            logging: true
+        }
+
+        return connection;
+    }
 })
