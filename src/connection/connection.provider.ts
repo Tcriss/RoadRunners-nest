ae661d8c0e78dc5294d96991a1dca00c5e2c404c
Cristian Tejeda <do.cristian.tr@hotmail.com>
2024-03-27 01:41:23 -0400

    fix: sintax error at connection.provider.ts

--- src/connection/connection.provider.ts
+++ src/connection/connection.provider.ts
@@ -7,7 +7,7 @@ import { Environment } from "../common/enums/environment.enum";
 export const ConnectionProvider: DynamicModule = TypeOrmModule.forRootAsync({
     inject: [ConfigService],
     async useFactory (config: ConfigService) {
-        const isDev: boolean = this.config.get('NODE_ENV') !== Environment.Production
+        const isDev: boolean = config.get('NODE_ENV') !== Environment.Production
         const connection: TypeOrmModuleOptions = {
             type: 'mongodb',
             url: config.get('DB_URI'),
