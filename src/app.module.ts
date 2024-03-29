a07f174fdb87780dfe92a1dd9c00441373b0e9b5
Cristian Tejeda <do.cristian.tr@hotmail.com>
2024-03-28 20:04:54 -0400

    chore: deleting unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { ConnectionModule } from './connection/connection.module';
+import { ConnectionProvider } from './connection/connection.provider';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
 import { UsersModule } from './modules/users/user.module';
 import { AuthModule } from './modules/auth/auth.module';
@@ -9,7 +9,7 @@ import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    ConnectionModule,
+    ConnectionProvider,
     VehiclesModule, 
     UsersModule,
     AuthModule,
--- src/connection/connection.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { ConnectionProvider } from './connection.provider';
-
-@Module({
-    imports: [ConnectionProvider],
-    exports: [ConnectionProvider]
-})
-export class ConnectionModule {}
